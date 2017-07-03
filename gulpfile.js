b8cff6bf7d736d4a0b4f0b16de01d3e0832f4b19
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-03 10:52:48 +0100

    remove trailing whitespace add gulp watch, to watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,46 +2,70 @@ const gulp = require("gulp");
 const eslint = require("gulp-eslint");
 const pug = require("gulp-pug");
 const sass = require("gulp-sass");
-const gulpIf = require("gulp-if");
+//const gulpIf = require("gulp-if");
+const imagemin = require("gulp-imagemin");
+
 const sourcemaps = require("gulp-sourcemaps");
 
-function isFormated(file) {
-    console.log(file.eslint.fixed);
-    return file.eslint !== null && file.eslint.fixed;
-}
 
 gulp.task("eslint", () => {
-    gulp.src("./src/**/*.js")
+    return gulp.src("./src/**/*.js")
         .pipe(eslint({
             fix: true,
             configFile: "./.eslintrc.json"
         }))
         .on("error", error => console.log(error))
-        .pipe(eslint.formatEach())
-        .pipe(gulpIf(isFormated, gulp.dest("./app/")));
+        .pipe(eslint.format())
+        .pipe(gulp.dest("./app/"))
+        .on("finish", () => {
+            console.log("done");
+        });
 });
 
 
 gulp.task("pug", () => {
     return gulp.src("src/renderer/pug/*.pug")
         .pipe(pug())
-        .pipe(gulp.dest("./app/"));
+        .on("error", error => console.log(error))
+        .pipe(gulp.dest("./app/renderer/html/"))
+        .on("finish", () => {
+            console.log("done");
+        });
 });
 
+gulp.task("imagemin", () => {
+    return gulp.src("./src/renderer/img/**/*")
+        .pipe(imagemin([
+            imagemin.svgo({plugins: [{removeViewBox: true}]})
+        ]))
+        .pipe(gulp.dest("./app/renderer/img/"));
+});
 
 gulp.task("sass", () => {
     return gulp.src("src/renderer/scss/*.scss")
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: "extended"}).on("error", sass.logError))
-        .pipe(sourcemaps.write("app/renderer/maps/"))
-        .pipe(gulp.dest("./app/"));
+        .pipe(sourcemaps.write("./maps"))
+        .pipe(gulp.dest("./app/renderer/css/"))
+        .on("finish", () => {
+            console.log("done");
+        });
 });
 
 
 gulp.task("watch", () => {
-    return gulp.watch(["**/**/js/*.js", "!node_modules/**", "app/renderer/code/scss/*.scss", "app/renderer/code/pug/*.pug"], ["eslint","pug", "sass"]);
-    
+
+    gulp.watch(["./src/**/*.js", "!node_modules/**"], [ "eslint" ]);
+
+    gulp.watch("./src/renderer/scss/*.scss", [ "sass" ]);
+
+    gulp.watch("./src/renderer/pug/*.pug", [ "pug" ]);
+
+    gulp.watch("./src/renderer/img", [ "imagemin" ]);
+
+    gulp.watch("./test.js", [ "log" ]);
+
 });
 
 
-gulp.task("default", [ "eslint", "pug", "sass" , "watch" ]);
+gulp.task("default", [ "eslint", "pug", "sass" , "imagemin", "watch" ]);
