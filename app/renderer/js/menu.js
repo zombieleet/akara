0a5bdf4ea4d41063d377cc67cf765816fc36f07a
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-03 15:10:57 +0100

    contextmenu menu

---
+++ app/renderer/js/menu.js
@@ -0,0 +1,53 @@
+( ({remote: { Menu, MenuItem, getCurrentWindow}}) => {
+
+    const akaraMenuToggle = document.querySelector(".akara-menu-toggle");
+
+    let menu = new Menu();
+
+    let menuItems = [
+        {
+            label: "Open"
+        },
+        {
+            label: "Load Playlist"
+        },
+        {
+            label: "Save"
+        },
+        {
+            type: "separator"
+        },
+        {
+            label: "Screenshot"
+        },
+        {
+            type: "separator"
+        },
+        {
+            label: "Play"
+        },
+        {
+            label: "Pause"
+        },
+        {
+            label: "Mute"
+        },
+        {
+            label: "Stop"
+        }
+    ];
+
+    
+    menuItems.forEach( items => {
+        menu.append(new MenuItem(items));
+    });
+
+    
+    /*akaraMenuToggle.addEventListener("click", event => {
+        menu.popup(getCurrentWindow());
+    });*/
+    
+    
+})(require("electron"));
+
+
