db0e7e650f2778f69db75608f65f00d7fd8c01da
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-04 16:50:58 +0100

    add dragging functionality to akara-loaded-dragger

--- src/renderer/js/akara_resize_loader.js
+++ src/renderer/js/akara_resize_loader.js
@@ -1,17 +1,40 @@
-; ((o,oP) => {
-    
-    o.addEventListener("dragstart", event => {
-        
-        console.log(event.x, event.y);
-        
-    });
+; ((o,oP,mCover) => {
 
-    oP.addEventListener("dragover", event => {
-        console.log(event.x, event.y, "dragover");
-    });
+
+
+    o.addEventListener("mousedown", initDrag ,false);
     
+    let startX, startWidth, startWidth2;
+    
+    function initDrag (event) {
+
+        startX = event.clientX;
+        console.log(startX);
+        startWidth = parseInt(document.defaultView.getComputedStyle(oP).width, 10);
+        startWidth2 = parseInt(document.defaultView.getComputedStyle(mCover).width, 10);
+
+        document.documentElement.addEventListener("mousemove", doDrag, false);
+        document.documentElement.addEventListener("mouseup", stopDrag, false);
+        
+    }
+
+    function doDrag(event) {
+        oP.setAttribute("style", `width: ${pxToPercent(startWidth + event.clientX - startX ,oP.parentNode.clientWidth)}%`);
+        mCover.setAttribute("style", `width: ${pxToPercent(startWidth2 + event.clientX - startX,mCover.parentNode.clientWidth)}%`);
+    }
+
+    function stopDrag(event) {
+        document.documentElement.removeEventListener("mousemove", doDrag, false);
+        document.documentElement.removeEventListener("mouseup", stopDrag, false);
+    }
+
+    function pxToPercent(target,context) {
+        return ( target / context ) * 100;
+    }
     
 })(
     document.querySelector(".akara-loaded-dragger"),
-    document.querySelector(".akara-load")
+    document.querySelector(".akara-load"),
+    document.querySelector(".akara-media-cover")
 );
+
