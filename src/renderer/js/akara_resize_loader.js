843163678e96de4612625d33183a2e7077995850
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-04 14:06:39 +0100

    fix draging of window

--- src/renderer/js/akara_resize_loader.js
+++ src/renderer/js/akara_resize_loader.js
@@ -1,1 +1,17 @@
-; (() => {});
+; ((o,oP) => {
+    
+    o.addEventListener("dragstart", event => {
+        
+        console.log(event.x, event.y);
+        
+    });
+
+    oP.addEventListener("dragover", event => {
+        console.log(event.x, event.y, "dragover");
+    });
+    
+    
+})(
+    document.querySelector(".akara-loaded-dragger"),
+    document.querySelector(".akara-load")
+);
