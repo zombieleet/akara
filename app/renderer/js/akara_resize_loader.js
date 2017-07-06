4dc2fc72c2b446acd3858e619f0f428529f2f1a5
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-06 05:49:40 +0100

    refactor of functions that handles resizing of akara-loaded

--- app/renderer/js/akara_resize_loader.js
+++ app/renderer/js/akara_resize_loader.js
@@ -1,40 +1,52 @@
-((o,oP,mCover) => {
-
-
-
-    o.addEventListener("mousedown", initDrag ,false);
+/*; ((o,oP,mCover,x) => {
     
     let startX, startWidth, startWidth2;
     
-    function initDrag (event) {
+    o.addEventListener("mousedown", initDrag ,false);
 
+    const pxToPercent = (target,context) => {
+        return ( target / context ) * 100;
+    };
+    
+    function initDrag (event) {
+        
         startX = event.clientX;
-        console.log(startX);
+        
         startWidth = parseInt(document.defaultView.getComputedStyle(oP).width, 10);
         startWidth2 = parseInt(document.defaultView.getComputedStyle(mCover).width, 10);
 
         document.documentElement.addEventListener("mousemove", doDrag, false);
         document.documentElement.addEventListener("mouseup", stopDrag, false);
-        
-    }
+    };
 
     function doDrag(event) {
-        oP.setAttribute("style", `width: ${pxToPercent(startWidth + event.clientX - startX ,oP.parentNode.clientWidth)}%`);
-        mCover.setAttribute("style", `width: ${pxToPercent(startWidth2 + event.clientX - startX,mCover.parentNode.clientWidth)}%`);
-    }
+        
+        let opWidth = pxToPercent(startWidth
+                                  + event.clientX
+                                  - startX,
+                                  oP.parentNode.clientWidth);
+        
+        let mCoverWidth = pxToPercent(startWidth2
+                                      + event.clientX
+                                      - startX,
+                                      mCover.parentNode.clientWidth);
+        
+        oP.setAttribute("style", `width: ${opWidth}%`);
+        mCover.setAttribute("style", `width: ${mCoverWidth}%`);
+        
+    };
 
-    function stopDrag(event) {
+    function stopDrag (event) {
         document.documentElement.removeEventListener("mousemove", doDrag, false);
         document.documentElement.removeEventListener("mouseup", stopDrag, false);
-    }
-
-    function pxToPercent(target,context) {
-        return ( target / context ) * 100;
-    }
+     };
+     
     
 })(
     document.querySelector(".akara-loaded-dragger"),
     document.querySelector(".akara-load"),
     document.querySelector(".akara-media-cover")
+    
 );
 
+*/
