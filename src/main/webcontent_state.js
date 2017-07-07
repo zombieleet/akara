ae8a59dc8442416801c808a1995946470e90f47f
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-07 13:28:45 +0100

    removal of checkType and iterateDir function to utils.js

--- src/main/webcontent_state.js
+++ src/main/webcontent_state.js
@@ -1,51 +1,7 @@
-const fs = require("fs");
-const { basename, join } = require("path");
+
 const { dialog , app } = require("electron");
 
-const checkType = path => {
-
-    if ( fs.statSync(path).isFile() ) {
-        return [ path ];
-    } else if ( fs.statSync(path).isDirectory() ) {
-        return iterateDir()(path);
-    } else {
-        return false;
-    }
-};
-
-const iterateDir = () => {
-    
-    const files = [],
-          dirs = [];
-
-    return function dirIt(directory) {
-        
-        try {
-
-            let dirContent = fs.readdirSync(directory);
-            
-            dirContent.forEach( path => {
-
-                const fullPath = join(directory,path);
-
-                if ( fs.statSync(fullPath).isFile() )
-                    files.push(fullPath);
-                else
-                    dirs.push(fullPath);
-            });
-
-            if ( dirs.length !== 0 )
-                
-                return dirIt(dirs.pop());
-                
-            return files;
-
-        } catch(ex) {
-            return false;
-        }        
-    }
-
-};
+const { checkType, iterateDir } = require("./utils.js");
 
 
 const handleWebContents = ({webContents}) => {
