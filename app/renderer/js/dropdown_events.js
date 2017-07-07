8a9ac19bd42866f52732420b339e256683420587
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-07 13:33:01 +0100

    addFolder function is created inother to handle the uploading of folder with media files in it

--- app/renderer/js/dropdown_events.js
+++ app/renderer/js/dropdown_events.js
@@ -1,14 +1,21 @@
-( ({remote: { dialog, app }}) => {
+( ({remote: { dialog, app , require: _require }}) => {
           
     const dropDownAkaraItem = document.querySelector(".akara-dropdown-item"),
           
-        { addMediaCb } = require("../js/dropdown_callbacks.js");
+        { addMediaCb } = require("../js/dropdown_callbacks.js"),
+          
+        { iterateDir } = _require("./utils.js"); // get utils from the main process folder
     
     
     dropDownAkaraItem.addEventListener("click", event => {
         
-        const target = event.target;
+        let target = event.target;
+        // incase a child of li is clicked
+        target = target.nodeName.toLowerCase() === "li" ? target : target.parentNode;
+
+        // incase the first check was sucessfull and the parentNode of target is not li;
         const targetDataDrop = target.hasAttribute("data-drop") ? target.getAttribute("data-drop") : undefined;
+        
         
         if ( ! targetDataDrop ) return undefined; 
         
@@ -22,7 +29,7 @@
     
     function HandleDroped() {
 
-        const addMedia = () => {
+        const addMediaFile = () => {
             
             dialog.showOpenDialog({
                 title: "Choose media file",
@@ -34,9 +41,28 @@
                 properties: ["openFile", "multiSelections"]
             },addMediaCb);
         };
+
+        const addMediaFolder = () => {
+
+            dialog.showOpenDialog({
+                title: "Choose Folder Containging Media Files",
+                defaultPath: app.getPath("videos"),
+                properties: [ "openDirectory", "multiSelections" ]
+            }, folderPaths => {
+
+                if ( ! folderPaths ) return ;
+
+                let files = [];
+
+                folderPaths.forEach( path => iterateDir()(path).forEach( filePath => files.push(filePath) ));
+                
+                addMediaCb(files);
+
+            });
+            
+        };
         
-        
-        return { addMedia };
+        return { addMediaFile, addMediaFolder };
     }
     
     
