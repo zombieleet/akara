972c100268570839e48ff4cfe8cdb55d7754f81d
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-07 13:27:42 +0100

    rename all __dirname to appPath which points to app.getAppPath() this was as a result of the fact that the __dirname now points to the new location of index.js

---
+++ src/main/index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const { app,  BrowserWindow } = require("electron");
+const trayMenu = require("./tray.js");
+const handleWinState = require("./window_state.js");
+const handleWebContents = require("./webcontent_state.js");
+const { join } = require("path");
+
+const appPath = app.getAppPath();
+
+require("electron-reload")(appPath, {
+    electron: join(appPath, "node_modules", ".bin", "electron")
+});
+
+const createWindow = () => {
+    
+    let mainWindow;
+
+    app.setName("Akara Media Player");
+    
+    app.setVersion(require(`${appPath}/package.json`).version);
+    
+    app.on("ready", () => {
+        
+        let tray = trayMenu();
+        // the backgroundColor property
+        //  is modified in the stylesheet
+        mainWindow = new BrowserWindow({
+            backgroundColor: "#4B4B4B",
+            frame: false,
+            show: false,
+            centre: true
+        });
+
+        mainWindow.loadURL(`file://${appPath}/app/renderer/html/index.html`);
+
+        mainWindow.on("ready-to-show", () => {
+            mainWindow.show();
+        });
+        
+        mainWindow.on("closed", () => {
+            mainWindow = undefined;
+            tray.destroy();
+            app.quit();
+        });
+
+        handleWebContents(mainWindow);        
+        handleWinState(mainWindow);
+    });
+};
+
+createWindow();
