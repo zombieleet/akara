758c4e20b5053c82439318f79b570d573662c088
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-07 13:28:12 +0100

    utilities copied from webcontents file to utils.js

---
+++ app/main/utils.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const { basename, join } = require("path");
+
+const checkType = path => {
+
+    if ( fs.statSync(path).isFile() ) {
+        return [ path ];
+    } else if ( fs.statSync(path).isDirectory() ) {
+        return iterateDir()(path);
+    } else {
+        return false;
+    }
+};
+
+const iterateDir = () => {
+    
+    const files = [],
+        dirs = [];
+
+    return function dirIt(directory) {
+        
+        try {
+            
+            let dirContent = fs.readdirSync(directory);
+            
+            dirContent.forEach( path => {
+
+                const fullPath = join(directory,path);
+
+                if ( fs.statSync(fullPath).isFile() )
+                    files.push(fullPath);
+                else
+                    dirs.push(fullPath);
+            });
+
+            if ( dirs.length !== 0 )
+                
+                dirIt(dirs.pop());
+            
+            return files;
+
+        } catch(ex) {
+            console.log(ex);
+            return false;
+        }        
+    };
+
+};
+
+module.exports = {
+    checkType,
+    iterateDir
+};
