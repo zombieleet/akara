2732a3ea5657ee55fe65d124297eb1b4e71d97ee
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-08 07:15:21 +0100

    create video list contextmenu

---
+++ src/main/menu.js
@@ -0,0 +1,84 @@
+const { nativeImage, ipcMain: ipc } = require("electron");
+const { join } = require("path");
+
+const videoListMenu = [
+    {
+        label: "Remove",
+        click(menuItem,{ webContents },event) {
+            
+            webContents.send("remove-target-hit");
+            
+        },
+        accelearation: "CtrlOrCommand+r"
+    },
+    { type: "separator" },
+    {
+        label: "Add To Playlist",
+        click() {
+        },
+        accelearation: "CtrlOrCommand+p"
+    },
+    {
+        label: "Play",
+        click() {
+        },
+        accelearation: "Alt+p"
+    },
+    {
+        label: "Pause",
+        click() {
+        },
+        accelearation: "Alt+o"
+    },
+    {
+        label: "Stop",
+        click() {
+        },
+        accelearation: "Alt+s"
+    },
+    {
+        label: "Previous",
+        click() {
+        },
+        accelearation: "p"
+    },
+    {
+        label: "Next",
+        click() {
+        },
+        accelearation: "n"
+    },
+    {
+        label: "Repeat",
+        click() {
+        }
+    },
+    {
+        type: "separator"
+    },
+    {
+        label: "Share",
+        submenu: [
+            {
+                label: "Facebook",
+                click() { },
+                accelearation: "CtrlOrCommand+1"
+            },
+            {
+                label: "Twitter",
+                click() { },
+                accelearation: "CtrlOrCommand+2"
+            },
+            {
+                label: "Youtube",
+                click() { },
+                accelearation: "CtrlOrCommand+3"
+            }
+        ]
+    }
+];
+
+
+module.exports = {
+    videoListMenu
+};
