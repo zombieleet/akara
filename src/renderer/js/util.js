c7ed118f62ae6d04a91479c11466023ec03a3fdb
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-08 07:14:52 +0100

    add few functions 1. removeTarget function < removes a targed video list element > 2. setCurrentPlaying < changes the style of the current playing video list element > 3. removeType < removes a particular attribute >

--- src/renderer/js/util.js
+++ src/renderer/js/util.js
@@ -1,19 +1,77 @@
+const { ipcRenderer: ipc } = require("electron");
+//const { basename, dirname } = require("path");
+
 const createEl = ({path: abs_path, _path: rel_path}) => {
-    
+
     const child = document.createElement("li");
     const childchild = document.createElement("span");
+
+    // nonsence
+    //abs_path = URL.createObjectURL( new File([ dirname(abs_path) ] , basename(abs_path)) );
     
-    child.setAttribute("data-full-path", abs_path);
-    
-    
+    child.setAttribute("data-full-path", encodeURI(abs_path));
+
+
     childchild.textContent = rel_path;
 
     child.appendChild(childchild);
-    
+
     return child;
+};
+
+const removeType = (pNode,...types) => {
+
+    Array.from(pNode.children, el => {
+
+        types.forEach( type => el.hasAttribute(type)
+                       ? el.removeAttribute(type)
+                       : "");
+
+    });
+};
+
+const setCurrentPlaying = target => {
+    
+    target.setAttribute("data-dbclicked", "true");
+    target.setAttribute("data-now-playing", "true");
+
+    target.classList.add("fa");
+    target.classList.add("fa-play-circle");
+
+    return ;
+};
+
+const RESETTARGET = target => target.nodeName.toLowerCase() === "li" ? target : target.parentNode;
+
+const removeTarget = (target,videoEl) => {
+
+    target = RESETTARGET(target);
+
+    
+    if ( videoEl.src.replace(/^file:\/\//,"") === target.getAttribute("data-full-path") ) {
+        
+        let _target = target.nextElementSibling || target.parentNode.firstElementChild;
+
+        if ( _target.parentNode.childElementCount === 1 ) {  
+            videoEl.src = "";
+        } else {
+            
+            videoEl.src = _target.getAttribute("data-full-path");
+
+            setCurrentPlaying(_target);
+        }
+        
+    }
+    
+    target.remove();
+    target = undefined;
 };
 
 
 module.exports = {
-    createEl
+    createEl,
+    removeTarget,
+
+    removeType,
+    setCurrentPlaying
 };
