657fcd4422505c766e10076eabd7200a0a547a64
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-07-16 12:17:52 +0100

    add prevNext function to handle the triggering of next media or previous media

--- src/renderer/js/util.js
+++ src/renderer/js/util.js
@@ -1,5 +1,6 @@
 const { ipcRenderer: ipc } = require("electron");
-//const { basename, dirname } = require("path");
+const url = require("url");
+const { video, controls: { play } } = require("../js/video_control.js");
 
 const createEl = ({path: abs_path, _path: rel_path}) => {
 
@@ -8,8 +9,12 @@ const createEl = ({path: abs_path, _path: rel_path}) => {
 
     // nonsence
     //abs_path = URL.createObjectURL( new File([ dirname(abs_path) ] , basename(abs_path)) );
-    
-    child.setAttribute("data-full-path", encodeURI(abs_path));
+
+    child.setAttribute("data-full-path", url.format({
+        protocol: "file",
+        slashes: "/",
+        pathname: abs_path
+    }));
 
 
     childchild.textContent = rel_path;
@@ -20,58 +25,151 @@ const createEl = ({path: abs_path, _path: rel_path}) => {
 };
 
 const removeType = (pNode,...types) => {
-
+    
     Array.from(pNode.children, el => {
 
         types.forEach( type => el.hasAttribute(type)
                        ? el.removeAttribute(type)
                        : "");
-
     });
 };
 
+const removeClass = (target, ...types) => {
+
+    Array.from(target.parentNode.children, el => {
+        for ( let i of types ) {
+            if ( el.classList.contains(i) ) el.classList.remove(i);
+        }
+    });
+
+};
+
 const setCurrentPlaying = target => {
-    
+
     target.setAttribute("data-dbclicked", "true");
     target.setAttribute("data-now-playing", "true");
 
+
+    target.setAttribute("data-clicked", "true");
+
     target.classList.add("fa");
     target.classList.add("fa-play-circle");
+
+    document.querySelector(".akara-title").textContent = target.querySelector("span").textContent;
 
     return ;
 };
 
 const RESETTARGET = target => target.nodeName.toLowerCase() === "li" ? target : target.parentNode;
 
-const removeTarget = (target,videoEl) => {
+const removeTarget = (target,video) => {
 
     target = RESETTARGET(target);
 
-    
-    if ( videoEl.src.replace(/^file:\/\//,"") === target.getAttribute("data-full-path") ) {
-        
+    if ( decodeURI(video.src) === target.getAttribute("data-full-path") ) {
+
         let _target = target.nextElementSibling || target.parentNode.firstElementChild;
 
-        if ( _target.parentNode.childElementCount === 1 ) {  
-            videoEl.src = "";
-        } else {
-            
-            videoEl.src = _target.getAttribute("data-full-path");
+        if ( _target.parentNode.childElementCount === 1 ) {
+            video.src = "";
+            video.removeAttribute("src");
 
+            const play = document.querySelector("[data-fire=play]");
+            const pause = document.querySelector("[data-fire=pause]");
+            pause.classList.add("akara-display");
+            play.classList.remove("akara-display");
+
+        } else {
+            video.src = _target.getAttribute("data-full-path");
             setCurrentPlaying(_target);
+            video.play();
         }
-        
+
     }
-    
+
     target.remove();
     target = undefined;
+    return ;
 };
 
+const __disable = (item,menuObject) => {
+
+    if (  item === menuObject.label ) {
+
+        menuObject.enabled = false;
+    }
+};
+
+
+const disableMenuItem = (memItem,target,video) => {
+
+    // if the label is play and the video is not paused
+    // disable the label
+    if ( memItem.label === "Play" && target.hasAttribute("data-now-playing") && ! video.paused)
+        memItem.enabled = false;
+
+
+    // if the label is pause and the video is paused
+    // disable the label
+
+    if ( memItem.label === "Pause" && target.hasAttribute("data-now-playing") && video.paused )
+        memItem.enabled = false;
+
+    // label is pause
+    // but the target is not currently been played , disabled the label
+    if ( memItem.label === "Pause" && ! target.hasAttribute("data-now-playing") )
+        memItem.enabled = false;
+
+    if ( memItem.label === "Repeat" && target.hasAttribute("data-repeat") )
+        memItem.visible = false;
+
+    if ( memItem.label === "No Repeat" && ! target.hasAttribute("data-repeat") )
+        memItem.visible = false;
+
+
+    if ( memItem.label === "Repeat All" && target.parentNode.hasAttribute("data-repeat") )
+        memItem.visible = false;
+
+    if ( memItem.label === "No Repeat All" && ! target.parentNode.hasAttribute("data-repeat") )
+        memItem.visible = false;
+
+
+};
+
+const setupPlaying = target => {
+
+    removeClass(target,"fa","fa-play-circle");
+
+    removeType(target.parentNode,"data-dbclicked","data-now-playing","data-clicked");
+
+    setCurrentPlaying(target);
+
+    video.src = target.getAttribute("data-full-path");
+
+    return play();
+
+};
+
+const prevNext = moveTo => {
+
+    let target = document.querySelector("[data-now-playing=true]");
+
+
+    if ( moveTo === "next" && target.nextElementSibling ) {
+        return setupPlaying(target.nextElementSibling);
+    }
+
+    if ( moveTo === "prev" && target.previousElementSibling )
+        return setupPlaying(target.previousElementSibling);
+};
 
 module.exports = {
     createEl,
     removeTarget,
-
+    removeClass,
     removeType,
-    setCurrentPlaying
+    setCurrentPlaying,
+    disableMenuItem,
+    setupPlaying,
+    prevNext
 };
