53ca58ace3cc98c38667d46079805579ad8aaa64
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-08-06 09:07:06 +0100

    creation of new windows, with dynamic options

---
+++ app/main/newwindow.js
@@ -0,0 +1,46 @@
+const {
+    BrowserWindow,
+    ipcMain: ipc
+} = require("electron");
+
+const {
+    APP_PATH,
+    BACKGROUND_COLOR
+} = require("./constants.js");
+
+const createNewWindow = (obj,html) => {
+
+    Object.assign(obj, {
+        frame: false,
+        show: false,
+        maximizable: false,
+        center: true,
+        backgroundColor: BACKGROUND_COLOR
+    });
+
+    let newWindow = new BrowserWindow(obj);
+
+    newWindow.loadURL(`file://${APP_PATH}/app/renderer/html/${html}`);
+
+    newWindow.on("closed", () => {
+        newWindow = undefined;
+    });
+    newWindow.on("ready-to-show", () => {
+        newWindow.show();
+    });
+
+    //parent.setIgnoreMouseEvents(true);
+    const { webContents } = newWindow;
+
+    webContents.openDevTools();
+
+    ipc.once(`close-${obj.title}-window`, () => {
+        newWindow.close();
+    });
+
+    return newWindow;
+};
+
+module.exports = {
+    createNewWindow
+};
