f5f2239bccd7c14c83d3b8029ce7d2d2f40a18bd
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-08-11 23:04:33 +0100

    add function to handle the exportation of CONVERTED_MEDIA

--- app/main/constants.js
+++ app/main/constants.js
@@ -1,16 +1,34 @@
+"use strict";
 const { join, basename } = require("path");
+
 const { app } = require("electron");
 
+const { existsSync,mkdirSync } = require("fs");
+
 const APP_PATH = app.getAppPath();
+
 const BACKGROUND_COLOR = "#4B4B4B";
+
 const USER_DATA = app.getPath("userData");
-const CONVERTED_MEDIA = join(USER_DATA, "converted_media");
+
+const _CONVERTED_MEDIA = () => {
+    const _conv = join(USER_DATA, "converted_media");
+    if ( existsSync(_conv) ) {
+        return _conv;
+    }
+    mkdirSync(_conv);
+    return _conv;
+};
+const CONVERTED_MEDIA = _CONVERTED_MEDIA();
+
 const DOWNLOADED_SUBTITLE = join(USER_DATA, "subtitle");
+
 const URL_ONLINE = "icanhazip.com/";
 
 const SIZE = 1000;
 
 const MEASUREMENT = [ "Bytes", "kB", "MB", "GB", "TB" ];
+
 /*const BYTE = 8;
 const MBYTE = 1048576;
 const GBYTE = 1073741824;
