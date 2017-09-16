69682460cf140105140b5646bafdcf537e553fa4
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-09-16 21:13:51 +0100

    preprocessed and transipled code

---
+++ app/renderer/js/tooltip.js
@@ -0,0 +1,55 @@
+( () => {
+
+    "use strict";
+
+    const docEl = document.documentElement;
+
+    docEl.addEventListener("mouseover", evt => {
+
+        const target = evt.target;
+
+        if ( ! target.hasAttribute("data-title") )
+            return false;
+
+        const toolTip = target.getAttribute("data-title");
+
+        const xAxis = evt.clientX;// - target.parentNode.getBoundingClientRect().left;
+        const yAxis = evt.clientY;// - target.parentNode.getBoundingClientRect().top;
+
+        const toolTipEl = document.createElement("span");
+
+        const ppNode = target.parentNode.parentNode;
+
+        const { left, top } = ppNode.getBoundingClientRect();
+        console.log(left);
+        toolTipEl.textContent = toolTip;
+
+        toolTipEl.setAttribute("class", "tool-tip");
+
+        toolTipEl.setAttribute(
+            "style",
+            `
+             position: absolute;
+             top: ${yAxis - top - 7}px ;
+             left: ${xAxis - left - 10}px;
+             z-index: 5555;
+           `
+        );
+
+        target.parentNode.parentNode.appendChild(toolTipEl);
+
+        return true;
+    });
+
+
+    docEl.addEventListener("mouseout", evt => {
+
+        let el;
+
+        if ( ( el = docEl.querySelector(".tool-tip") ) )
+            el.remove();
+
+        return ;
+    });
+
+})();
