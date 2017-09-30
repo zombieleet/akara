0c8088544cf99bfc79f144937241fc2bb5da7924
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-09-30 12:47:23 +0100

    linting

--- src/main/constants.js
+++ src/main/constants.js
@@ -21,27 +21,44 @@ const BACKGROUND_COLOR = "#4B4B4B";
 const USER_DATA = app.getPath("userData");
 
 const PLAYLIST_FILE = () => {
+
     const playlistfile = join(USER_DATA, "playlist.json");
-    if ( existsSync(playlistfile) ) {
+
+    if ( existsSync(playlistfile) )
         return playlistfile;
-    }
+
     writeFileSync(playlistfile, JSON.stringify({}));
+
     return playlistfile;
 };
 
 const _CONVERTED_MEDIA = () => {
     const _conv = join(USER_DATA, "converted_media");
-    if ( existsSync(_conv) ) {
+
+    if ( existsSync(_conv) )
         return _conv;
-    }
-    
+
     mkdirSync(_conv);
+
     return _conv;
 };
 
+const _CURRENT_TIME = () => {
+
+    const currenttime = join(USER_DATA, "currenttime");
+
+    if ( existsSync(currenttime) )
+        return currenttime;
+
+    mkdirSync(currenttime);
+
+    return currenttime;
+};
 const CONVERTED_MEDIA = _CONVERTED_MEDIA();
 
 const DOWNLOADED_SUBTITLE = join(USER_DATA, "subtitle");
+
+const CURRENT_TIME = _CURRENT_TIME();
 
 const URL_ONLINE = "icanhazip.com/";
 
@@ -57,6 +74,7 @@ const TBYTE = 1099511627776;
 
 module.exports = {
     CONVERTED_MEDIA,
+    CURRENT_TIME,
     APP_PATH,
     BACKGROUND_COLOR,
     URL_ONLINE,
