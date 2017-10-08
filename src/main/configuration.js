21439bf70b2ccc273ee42ead92a27d5198fdb584
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-10-08 15:16:13 +0100

    add PODCAST() to the list of config object exported to the renderer process

--- src/main/configuration.js
+++ src/main/configuration.js
@@ -1,14 +1,17 @@
 const {
-    PLAYLIST_FILE
+    PLAYLIST_FILE,
+    PODCAST
 } = require("./constants.js");
 
 const SHORTCUT_KEYS = require("./shotcutkeys.json");
 
 const playlistFile = PLAYLIST_FILE();
+const podcast = PODCAST();
 
 module.exports = {
     playlist: {
         file: playlistFile
     },
+    podcast,
     shortcut: SHORTCUT_KEYS
 };
