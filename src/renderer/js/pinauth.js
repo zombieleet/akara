07e5a0366fc8f5f29a444ceaf02c4a49b4c472c9
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-10-16 16:04:07 +0100

    add support for authenticating youtube pin

--- src/renderer/js/pinauth.js
+++ src/renderer/js/pinauth.js
@@ -3,7 +3,7 @@
     const inputPin = document.querySelector(".inputpin-input");
     const share = document.querySelector(".inputpin-pin");
     const close = document.querySelector(".inputpin-close");
-    
+
     const {
         remote: {
             dialog,
@@ -13,23 +13,72 @@
     } = require("electron");
 
     const {
-        tClient
+        tClient,
+        youtubeClient,
+        cache,
+        uploadVideo
     } = require("../js/util.js");
+
+    const fs = require("fs");
+
+    const cacheToken = token =>
+          fs.writeFileSync(cache, JSON.stringify(token));
     
+    const validateAuth = Object.defineProperties({}, {
+        twitter: {
+            value(pin) {
+                console.log(pin);
+                tClient.get("oauth/access_token", { oauth_verifier: pin }, (...args) => {
+                    console.log(args);
+                });
+            }
+        },
+        youtube: {
+            value(pin) {
+                // code goes here
+                youtubeClient.getToken( pin, (err,token) => {
+                    if ( err )
+                        return dialog.showErrorBox(
+                            "Error validiating pin",
+                            `${pin} cannot be validated`
+                        );
+                    
+                    youtubeClient.credentials = token;
+                    
+                    cacheToken(token);
+                    
+                    return uploadVideo(youtubeClient);
+                    
+                });
+            }
+        },
+        facebook: {
+            value(pin) {
+                // code goes here
+                console.log(pin);
+            }
+        }
+    });
+
     share.addEventListener("click", evt => {
-        
+
         const pin = inputPin.value;
-        
+
         if ( /^\s{0,}$/.test(pin) )
             return dialog.showErrorBox(
                 "No Pin",
                 "Enter a pin in the input pin field"
             );
 
-        tClient.get("oauth/access_token", { oauth_verifier: pin }, (...args) => {
-            console.log(args);
-        });
-        
+        const {
+            browserWindowOptions: {
+                __akaraType
+            }
+        } = getCurrentWindow()
+              .getParentWindow()
+              .webContents;
+
+        return validateAuth[__akaraType](pin);
     });
 
     close.addEventListener("click", evt => getCurrentWindow().close());
