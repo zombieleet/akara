09d6455aa083892b8db2f56e5c5bffc83602d42e
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-10-26 15:43:49 +0100

    change the top and left location where tooltip should show

--- src/renderer/js/tooltip.js
+++ src/renderer/js/tooltip.js
@@ -9,36 +9,23 @@
         const target = evt.target;
 
         if ( ! target.hasAttribute("data-title") )
-            
             return false;
 
         const toolTip = target.getAttribute("data-title");
-
         const xAxis = evt.clientX;// - target.parentNode.getBoundingClientRect().left;
         const yAxis = evt.clientY;// - target.parentNode.getBoundingClientRect().top;
-
         const toolTipEl = document.createElement("span");
-
         const ppNode = target.parentNode.parentNode;
-
         const { left, top } = ppNode.getBoundingClientRect();
-
+        
         toolTipEl.textContent = toolTip;
-
         toolTipEl.setAttribute("class", "tool-tip");
-
-        toolTipEl.setAttribute(
-            "style",
-            `
-             position: absolute;
-             top: ${yAxis - top - 7}px ;
-             left: ${xAxis - left - 10}px;
-             z-index: 5555;
-           `
-        );
-
+        toolTipEl.style.position = "absolute";
+        toolTipEl.style.top = `${yAxis - top + target.offsetHeight}px`;
+        toolTipEl.style.left = `${xAxis - left - 50}px`;
+        toolTipEl.style["z-index"] = 5555;
         target.parentNode.parentNode.appendChild(toolTipEl);
-
+        
         return true;
     });
 
