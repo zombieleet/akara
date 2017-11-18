a133c5b158541f00a51efa96f702a0096b8fef59
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-11-18 03:53:44 +0100

    add support for removing and reseting of posters

--- src/renderer/js/settings/poster.js
+++ src/renderer/js/settings/poster.js
@@ -26,10 +26,19 @@
     const checkBox = document.querySelector(".poster-albumart-checkbox");
     const close = document.querySelector(".poster-close");
     const install = document.querySelector(".poster-install");
-
+    const reset = document.querySelector(".poster-reset");
+    const remove= document.querySelector(".poster-remove");
+    
+    const posterPath = path.join(
+        app.getAppPath(),
+        "app",
+        "renderer",
+        "img",
+        "posters"
+    );
 
     const createPosterContainer = posterPath => {
-        
+
         const li = document.createElement("li");
         const img = new Image(100,100);
 
@@ -43,7 +52,7 @@
         img.src = posterPath;
         img.draggable = false;
 
-        
+
         li.addEventListener("click", () => {
             Array.from(document.querySelectorAll(".poster-list-item"), el => {
                 if ( el.hasAttribute("data-current-value") )
@@ -52,31 +61,32 @@
 
             li.setAttribute("data-current-value", "true");
             ipc.sendTo(1, "akara::video:poster:change", posterPath);
-        });        
+        });
 
         return li;
     };
-    
+
     const appendPostersToDom = value => {
-        
+
         const posterList = document.querySelector(".poster-list");
-        
+
         value.forEach( async (_poster) => {
 
             const { poster } = require(await requireSettingsPath("poster.json"));
 
             const li = createPosterContainer(_poster);
-            
+
             if ( poster === _poster )
                 li.setAttribute("data-current-value", "true");
 
             posterList.appendChild(li);
-        });        
+        });
     };
-    
+
     const loadPosters = async () => {
 
-        const pathToSystemPosters = path.resolve(path.join(__dirname, "../../../img/posters"));
+        const pathToSystemPosters = posterPath;
+
         const pathToUserPosters = user_poster_location;
 
         let value ;
@@ -96,17 +106,19 @@
             value = ex;
         }
 
-        if ( Error[Symbol.hasInstance](value) )
+        if ( Error[Symbol.hasInstance](value) ) {
+            console.log(value);
             return dialog.showErrorBox("Error Loading Posters","Cannot load Posters");
+        }
 
         return value;
     };
 
     const saveSettings = async (key,value) => {
-        
+
         const posterJson = await requireSettingsPath("poster.json");
         const posterSettings = require(posterJson);
-        
+
         Object.assign(posterSettings, {
             [key]: value
         });
@@ -114,7 +126,7 @@
         fs.writeFileSync(posterJson, JSON.stringify(posterSettings));
     };
 
-    
+
     const loadSettingsPreference = async () => {
         const posterJson = await requireSettingsPath("poster.json");
         const posterSettings = require(posterJson);
@@ -122,29 +134,29 @@
 
         checkBox.checked = posterSettings.album_art ? true : false;
     };
-    
+
     window.addEventListener("DOMContentLoaded",loadSettingsPreference);
-    
+
     save.addEventListener("click", async (evt) => {
-        
+
         Array.from(document.querySelectorAll(".poster-list-item"), el => {
             if ( ! el.hasAttribute("data-current-value") )
                 return ;
             const url = el.getAttribute("data-poster-path");
             saveSettings("poster", url);
         });
-        
+
         if ( checkBox.checked )
             return saveSettings("album_art", true);
-        
+
         return saveSettings("album_art", false);
     });
 
-    
+
     close.addEventListener("click", () => getCurrentWindow().close());
 
     install.addEventListener("click", () => {
-        
+
         dialog.showOpenDialog({
             defaultPath: app.getPath("pictures"),
             properties: [ "openFile", "multiSelection" ],
@@ -156,16 +168,16 @@
                 { name: "bmp" , extensions: [ "bmp" ] }
             ]
         }, paths => {
-            
+
             if ( ! paths )
                 return ;
 
             const posterList = document.querySelector(".poster-list");
-            
+
             paths.forEach( imagePath => {
 
                 const imgRelPath = path.basename(imagePath);
-                
+
                 const liDom = document.querySelector(".poster-list-item");
                 const li = createPosterContainer(imagePath);
 
@@ -175,10 +187,100 @@
                 copyImgFrom.on("end", () => {
                     posterList.insertBefore(li,liDom);
                 });
-                               
+
                 copyImgFrom.pipe(copyImgTo,{ end: false });
             });
         });
     });
 
+    reset.addEventListener("click", async () => {
+        const posterJson = await requireSettingsPath("poster.json");
+        const posterSettings = require(posterJson);
+
+        const posterListItem = Array.from(document.querySelectorAll(".poster-list-item")); //el => {
+        
+        let posterPath ;
+        
+        for ( let el of posterListItem ) {
+
+            if ( el.hasAttribute("data-current-value") )
+                el.removeAttribute("data-current-value");
+
+            if ( (posterPath = el.getAttribute("data-poster-path")) ) {
+                if (  path.basename(posterPath) === "default_poster.jpg" ) {
+                    el.setAttribute("data-current-value", true);
+                    break;
+                }
+            }
+        }
+        
+        checkBox.checked = false;
+        saveSettings("poster", posterPath);
+        saveSettings("album_art", false);
+        ipc.sendTo(1, "akara::video:poster:change", posterPath);
+    });
+
+    remove.addEventListener("click", () => {
+        
+        const posterListItem = Array.from(document.querySelectorAll(".poster-list-item"));
+        const pathToUserPosters = user_poster_location;
+        
+        let found ;
+        let _el;
+        
+        for ( let el of posterListItem ) {
+            if ( el.hasAttribute("data-current-value") ) {
+                
+                let dataFrom = el.getAttribute("data-poster-path");
+                
+                if ( path.dirname(dataFrom) !==  posterPath ) {
+                    _el = el;
+                    found = 1;
+                    break;
+                } else {
+                    found = 0;
+                }
+            }
+        }
+
+        if ( found === 0 ) {
+            dialog.showErrorBox(
+                "Remove Poster",
+                "This Poster is a system poster, it can't be removed"
+            );
+            return ;
+        }
+        
+        if ( found === 1 ) {
+            
+            const posterToRemove = _el.getAttribute("data-poster-path");
+            
+            fs.readdir(pathToUserPosters, (err,info) => {
+                
+                if ( err )
+                    return err;
+                
+                let location ;
+                
+                if ( (location = info.indexOf(path.basename(posterToRemove))) >= 0 ) {
+                    
+                    fs.unlink(path.join(pathToUserPosters,info[location]), err => {
+                        if ( err ) {
+                            console.log(err);
+                            return dialog.showErrorBox("Cannot Remove Poster", "Unable to remove poster");
+                        }
+                        
+                        return _el.remove();
+                    });
+                    
+                }
+
+                return true;
+            });
+            
+        }
+
+        return ;
+    });
+    
 })();
