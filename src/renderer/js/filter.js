fef6403ccf77fd61634cc300e587cc5360da9808
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-12-09 12:17:35 +0100

    remove the use of localStorage to store filters settings in favour for using flat files

--- src/renderer/js/filter.js
+++ src/renderer/js/filter.js
@@ -1,35 +1,34 @@
 ; ( () => {
     "use strict";
-    
-    window.addEventListener("DOMContentLoaded", () => {
-
-        let allFilters = localStorage.getItem("filters");
-
-        if ( ! allFilters )
-            return ;
-
-        allFilters = JSON.parse(allFilters);
-
-        Object.keys(allFilters).forEach( filter => {
-            const filterType = document.querySelector(`[data-filter-type=${filter}]`);
-            const progressBar = filterType.querySelector(".progress-bar");
-            progressBar.style.width = allFilters[filter].progressBarWidth + "%";
-        });
-    });
 
     const {
         ipcRenderer: ipc,
         remote: {
-            getCurrentWindow
+            getCurrentWindow,
+            require: _require
         }
     } = require("electron");
 
+    const { requireSettingsPath } = _require("./constants.js");
+    const fs = require("fs");
+    
     const akara_emit = require("../js/emitter.js");
+    
 
     const progress = document.querySelector("progress");
 
-    const handleFilters = (evt,dir,cb) => {
 
+    const saveFilters = async (filterConfig) => {
+        const filterSettingsPath = await requireSettingsPath("filter.json");
+        const filterSettings = require(filterSettingsPath);
+
+        Object.assign(filterSettings, filterConfig);
+        
+        fs.writeFileSync(filterSettingsPath, JSON.stringify(filterSettings));
+    };
+
+    const handleFilters = async (evt,dir,cb) => {
+        
         const target = evt.target;
         const pNode = target.parentNode;
 
@@ -48,63 +47,63 @@
             : parseInt(progressBar.style.width);
         
         progressBar.style.width = ( dir === "incr"  ? progressBarWidth + 1 : progressBarWidth  - 1 ) + "%";
-
+        
         if ( filterType === "brightness")
             progressBarWidth /= 10;
         
         ipc.sendTo(1, "akara::video:filter", { filterType, progressBarWidth, measurement });
-
-        let prevFilters = localStorage.getItem("filters");
-
-        if ( ! prevFilters ) {
-            localStorage.setItem("filters", JSON.stringify( { [filterType]: { progressBarWidth } } ));
-            return ;
-        }
-
-        prevFilters = Object.assign(JSON.parse(prevFilters), {
-            [filterType]: { progressBarWidth }
-        });
         
-        localStorage.setItem("filters", JSON.stringify(prevFilters));
+        saveFilters({ [filterType]: { progressBarWidth, measurement } });
+        
         return ;
     };
 
-    const resetFilters = (evt) => {
+    const resetFilters = async (evt) => {
+        
+        const filterSettingsPath = await requireSettingsPath("filter.json");
+        const filterSettings = require(filterSettingsPath);
+        
         let target = evt.target;
         let pNode = target.parentNode;
         let progressBar = pNode.querySelector(".progress-bar");
 
-        let allFilters = localStorage.getItem("filters");
-
-        if ( ! allFilters )
-            return ;
-
-        allFilters = JSON.parse(allFilters);
         
         let filterType = pNode.getAttribute("data-filter-type");
         
-        if ( Object.keys(allFilters).indexOf(filterType) >= 0 ) {
-            delete allFilters[filterType];
-            localStorage.setItem("filters", JSON.stringify(allFilters));
+        if ( Object.keys(filterSettings).indexOf(filterType) >= 0 ) {
+            
+            /**TODO: 
+               do not DELETE but RESET to default html5 values of video filters
+            **/
+            delete filterSettings[filterType];
+            
             progressBar.style.width = null;
+            
             ipc.sendTo(1,"akara::video:filter:reset", filterType);
+            
+            saveFilters(filterSettings);
         }
     };
 
-    const resetAllFilters = evt => {
+    const resetAllFilters = async (evt) => {
 
-        let allFilters = localStorage.getItem("filters");
-
-        if ( ! allFilters )
-            return ;
-
-        Object.keys(JSON.parse(allFilters)).forEach( filter => {
+        const filterSettingsPath = await requireSettingsPath("filter.json");
+        const filterSettings = require(filterSettingsPath);
+        
+        Object.keys(filterSettings).forEach( filter => {
+            
             let filterType = document.querySelector(`[data-filter-type=${filter}]`);
             filterType.querySelector(".progress-bar").style.width = null;
+
+            /**TODO: 
+               do not DELETE but RESET to default html5 values of video filters
+            **/
+            delete filterSettings[filterType];
+            saveFilters(filterSettings);
         });
         
         ipc.sendTo(1,"akara::video:filter:reset:all");
-        localStorage.removeItem("filters");
+        
     };
     
     document.body.addEventListener("mousedown", evt => {
@@ -127,4 +126,17 @@
     document.querySelector(".filter-close")
         .addEventListener("click", () => getCurrentWindow().close());
 
+        
+    window.addEventListener("DOMContentLoaded", async () => {
+        
+        const filterSettingsPath = await requireSettingsPath("filter.json");
+        const filterSettings = require(filterSettingsPath);
+
+        Object.keys(filterSettings).forEach( filter => {
+            const filterType = document.querySelector(`[data-filter-type=${filter}]`);
+            const progressBar = filterType.querySelector(".progress-bar");
+            progressBar.style.width = filterSettings[filter].progressBarWidth + "%";
+        });
+    });
+    
 })();
