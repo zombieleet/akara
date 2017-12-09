ad7dadc27a139b7803da78b6a88ac75fa4e37321
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-12-09 12:17:04 +0100

    modify script to download all platform binary of ffmpeg for easy packaging

--- download-ffmpeg.js
+++ download-ffmpeg.js
@@ -1,20 +1,37 @@
 const ffbinaries = require("ffbinaries");
 
+const fs = require("fs");
+const path = require("path");
+
 const DESTINATION = `${__dirname}/node_modules/.bin/`;
 
-const download = [ "ffmpeg", "ffserver", "ffplay", "ffprobe" ];
+const platforms = [ "linux-64", "linux-32", "linux-armhf", "linux-armel", "osx", "win-32", "win-64" ];
 
-function downloadBin(arg) {
-    if ( ! arg ) {
-        ffbinaries.downloadFiles(download, { quiet: false, destination: DESTINATION }, () => {
-            console.log("Downloaded ffmpeg to %s", DESTINATION);
-        });
-        return ;
-    }
+function downloadBin() {
 
-    ffbinaries.downloadFiles(download, { platform: arg, quiet: false, destination: DESTINATION  }, () => {
-        console.log("Downloaded ffmpeg to %s", DESTINATION);
+    platforms.forEach( plat => {
+        
+        ffbinaries.downloadFiles("ffmpeg",{ platform: plat, quiet: false, destination: DESTINATION }, (err,data) => {
+            
+            console.log("Downloaded %s for %s " , JSON.stringify(data), plat);
+            
+            let _path = path.join(DESTINATION,"ffmpeg") ;
+            
+            if ( ! fs.existsSync(_path) )
+                _path = path.join(DESTINATION,"ffmpeg.exe");
+            
+            fs.rename(
+                _path,
+                _path.endsWith(".exe")
+                    ? path.join(DESTINATION,`ffmpeg-${plat}.exe`)
+                    : path.join(DESTINATION,`ffmpeg-${plat}`) 
+                    ,
+                err => {
+                    console.log( err ? err : "renamed");
+                });
+        }); 
     });
+    
 }
 
-downloadBin(process.argv.slice(2)[0]);
+downloadBin();
