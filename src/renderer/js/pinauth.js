3ad29243b6da99aa90f0aa57e78edebb4a18e2e6
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-12-23 14:08:14 +0100

    implementation of all settings option

--- src/renderer/js/pinauth.js
+++ src/renderer/js/pinauth.js
@@ -5,10 +5,12 @@
     const close = document.querySelector(".inputpin-close");
 
     const {
+        ipcRenderer: ipc,
         remote: {
             dialog,
             BrowserWindow,
-            getCurrentWindow
+            getCurrentWindow,
+            require: _require
         }
     } = require("electron");
 
@@ -19,33 +21,48 @@
         uploadYoutubeVideo
     } = require("../js/util.js");
 
+    const {
+        requireSettingsPath
+    } = _require("./constants.js");
+
     const fs = require("fs");
 
-    const cacheToken = token =>
-          fs.writeFileSync(cache, JSON.stringify(token));
-    
+    const cacheToken = (token,expiry_date) => {
+        token.expiry_date = expiry_date;
+        fs.writeFileSync(cache, JSON.stringify(token));
+    };
+
     const validateAuth = Object.defineProperties({}, {
         twitter: {
             value(pin) {
-                console.log(pin);
                 tClient.get("oauth/access_token", { oauth_verifier: pin }, (...args) => {
                     console.log(args);
                 });
             }
         },
         youtube: {
-            value(pin) {
-                // code goes here
+            async value(pin) {
+
+                const shareFile = await requireSettingsPath("share.json");
+                const shareSettings = require(shareFile);
+
                 youtubeClient.getToken( pin, (err,token) => {
+                    
                     if ( err )
                         return dialog.showErrorBox(
                             "Error validiating pin",
                             `${pin} cannot be validated`
                         );
-                    
+
                     youtubeClient.credentials = token;
-                    cacheToken(token);
-                    return uploadYoutubeVideo(youtubeClient);
+
+                    if ( shareSettings.cache_authentication_request === "yes" )
+                        cacheToken(token,shareSettings.cache_expire_date);
+
+                    getCurrentWindow().getParentWindow().close();
+                    ipc.sendTo(1, "akara::youtube:loggedin:share", youtubeClient);
+
+                    return true;
                 });
             }
         },
