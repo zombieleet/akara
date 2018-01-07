28007a4835dff0885289000646ba331ce0993ac1
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-01-07 03:18:18 +0100

    simplify reading of tags

--- src/renderer/js/mediainfo.js
+++ src/renderer/js/mediainfo.js
@@ -12,36 +12,25 @@
 
     const url = require("url");
 
-    const { handleWindowButtons } = require("../js/util.js");
+    const {
+        handleWindowButtons,
+        getMetaData
+    } = require("../js/util.js");
 
     const mediaMin = document.querySelector(".window-min");
     const mediaMax = document.querySelector(".window-max");
     const mediaClose = document.querySelector(".window-close");
 
-    const getAllTags = () => {
+    
+    ipc.sendTo(1, "akara::video::currentplaying", getCurrentWindow().webContents.id);
+    
+    ipc.on("akara::video::currentplaying:src", async ( evt, data ) => {
+        console.log(decodeURIComponent(url.parse(data).path));
+        await getMetaData(decodeURIComponent(url.parse(data).path));
+    });
 
-        const jsmediaTags = require("jsmediatags");
-        
-        ipc.sendTo(1, "akara::video::currentplaying");
-        
-        ipc.once("akara::video::currentplaying:src", ( evt, src ) => {
 
-            const mediaTagReader = new jsmediaTags.Reader(
-                decodeURIComponent(url.parse(src).path)
-            );
-
-            mediaTagReader.setTagsToRead(["picture"])
-                .read({
-                    onSuccess({ tags }) {
-                        console.log(tags);
-                    },
-                    onError(error) {
-                        console.log(error);
-                    }
-                });
-        });
-    };
-
+    
     handleWindowButtons({ close: mediaClose, min: mediaMin, max: mediaMax });
 
 })();
