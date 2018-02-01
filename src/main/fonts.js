5e4c48f16eee98b16bc8c5802c7a57b02e53df02
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-02-01 03:31:00 +0100

    an array of builtin fonts

---
+++ src/main/fonts.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const play = [ "fa-play", "fa-play-circle", "fa-play-circle-o" ];
+const stop = [ "fa-stop", "fa-stop-circle", "fa-stop-circle-o" ];
+const backward = [ "fa-backward", "fa-fast-backward", "fa-step-backward" ];
+const forward = [ "fa-fast-foward", "fa-foward", "fa-step-forward" ];
+
+const filter = [ ];
+const random = [ ];
+const expand = [ ];
+const shrink = [ ];
+
+
+module.exports = {
+    play,
+    stop,
+    backward,
+    forward,
+    filter,
+    random,
+    expand,
+    shrink
+};
