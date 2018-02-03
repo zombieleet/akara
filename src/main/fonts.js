3f45048b8a7d4fdad0b903e420f6b187707ac7c0
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-02-03 12:37:15 +0100

    add more font

--- src/main/fonts.js
+++ src/main/fonts.js
@@ -1,23 +1,47 @@
 "use strict";
 
-const play = [ "fa-play", "fa-play-circle", "fa-play-circle-o" ];
-const stop = [ "fa-stop", "fa-stop-circle", "fa-stop-circle-o" ];
-const backward = [ "fa-backward", "fa-fast-backward", "fa-step-backward" ];
-const forward = [ "fa-fast-foward", "fa-foward", "fa-step-forward" ];
+module.exports.play = [ "fa-play", "fa-play-circle", "fa-play-circle-o" ];
+module.exports.stop = [ "fa-stop", "fa-stop-circle", "fa-stop-circle-o" ];
+module.exports.backward = [ "fa-backward", "fa-fast-backward", "fa-step-backward" ];
+module.exports.forward = [ "fa-fast-foward", "fa-foward", "fa-step-forward" ];
+module.exports.check = [ "fa-check", "fa-check-circle", "fa-check-square", "fa-check-square-o" ];
+module.exports.uncheck = [ "fa-square", "fa-square-o", "fa-circle", "fa-circle-o", "fa-circle-o-notch", "fa-circle-thin"];
+module.exports.pause = [ "fa-pause", "fa-pause-circle", "fa-pause-circle-o" ];
 
-const filter = [ ];
-const random = [ ];
-const expand = [ ];
-const shrink = [ ];
+module.exports.home = [ "fa-home" ];
+module.exports.list = [ "fa-list", "fa-list-alt", "fa-list-ol", "fa-list-ul" ];
+module.exports.grid = [ "fa-th-larget" , "fa-th" ];
+
+module.exports.close = [ "fa-window-close", "fa-window-close-o", "fa-close" ];
+module.exports.folder = [ "fa-folder", "fa-folder-o", "fa-folder-open", "fa-folder-open-o" ];
+module.exports.times = [ "fa-times-rectangle", "fa-times-rectangle-o", "fa-times", "fa-times-circle", "fa-times-circle-o" ];
+
+module.exports.download = [ "fa-download", "fa-cloud-download" ];
 
 
-module.exports = {
-    play,
-    stop,
-    backward,
-    forward,
-    filter,
-    random,
-    expand,
-    shrink
-};
+module.exports.filter = [ ];
+module.exports.random = [ ];
+module.exports.expand = [ ];
+module.exports.shrink = [ ];
+
+
+// module.exports = {
+//     play,
+//     stop,
+//     backward,
+//     forward,
+//     filter,
+//     random,
+//     expand,
+//     shrink,
+//     pause,
+//     home,
+//     grid,
+//     list,
+//     times,
+//     download,
+//     close,
+//     folder,
+//     uncheck,
+//     check
+// };
