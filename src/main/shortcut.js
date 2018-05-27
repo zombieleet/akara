a06faa6c1dbab8df98feb1e5beb77353fd14895b
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-05-27 19:50:48 +0100

    add all media related settings to media array of objects

--- src/main/shortcut.js
+++ src/main/shortcut.js
@@ -1,5 +1,5 @@
 
-module.exports.video = [
+module.exports.media = [
     { fullscreen: { key: "f", modifier: []} },
     { next:  { key: "n", modifier: []} },
     { previous: { key: "p", modifier: []} },
@@ -9,10 +9,7 @@ module.exports.video = [
     { "previous frame slow": { key: "ArrowLeft", modifier: [ "shiftKey" ] }},
     { "next frame fast": { key: "ArrowRight", modifier: [ "ctrlKey" ] }},
     { "previous frame fast": { key: "ArrowLeft", modifier: [ "ctrlKey" ] }},
-    { "play searched media": { key: "Enter", modifier: [] } }
-];
-
-module.exports.audio = [
+    { "play searched media": { key: "Enter", modifier: [] } },
     { "play and pause": { key: "Space", modifier: []} },
     { "volume up": { key: "ArrowUp", modifier: [ "ctrlKey" ] } },
     { "volume down": { key: "ArrowDown", modifier: [ "ctrlKey" ] } },
@@ -21,18 +18,14 @@ module.exports.audio = [
     { "very fast playback rate": { key: "f", modifier: [ "shiftKey" ] } },
     { "slow playback rate": { key: "s", modifier: [ "altKey", "ctrlKey" ] } },
     { "very slow playback rate": { key: "s", modifier: [ "shiftKey" ] } },
-];
-
-module.exports.subtitle = [
-    { "subtitle computer": { key: "t" , modifier: [ "altKey" ] } },
-    { "subtitle internet": { key: "x", modifier: [ "altKey" ] } }
-];
-
-module.exports.others = [
     { "open media file": { key: "f", modifier: ["altKey"] } },
     { "open media folder": { key: "f", modifier: ["altKey","shiftKey"] } },
     { "show search box": { key: "s", modifier: ["ctrlKey"] } },
     { "open media file location": { key: "o", modifier: [ "ctrlKey" ] } },
     { "media info" : { key: "m", modifier: [ "ctrlKey", "shiftKey"] } },
-    { "media info" : { key: "m", modifier: [ "ctrlKey", "shiftKey"] } },
+];
+
+module.exports.subtitle = [
+    { "subtitle computer": { key: "t" , modifier: [ "altKey" ] } },
+    { "subtitle internet": { key: "x", modifier: [ "altKey" ] } }
 ];
