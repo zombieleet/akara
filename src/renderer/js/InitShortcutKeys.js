c8ee1f6d68995116b43de3ba21d7873508d915b0
Victory Osikwemhe <zombieleetnca@gmail.com>
2018-06-11 04:50:18 +0100

    add support for sending signal to the settings window to change it's shortcut key

--- src/renderer/js/InitShortcutKeys.js
+++ src/renderer/js/InitShortcutKeys.js
@@ -1,12 +1,15 @@
 ; ( () => {
-    
+
 
     const {
-        ipcRenderer: ipc
+        ipcRenderer: ipc,
+        remote: {
+            BrowserWindow
+        }
     } = require("electron");
 
     console.log("pandroa's box");
-    
+
     const settingsShortCutKey = require("../js/ShortCutKeys/SettingsShortCutKey.js");
     const mediaShortCutKey = require("../js/ShortCutKeys/MediaShortCutKey.js");
     const windowShortCutKey = require("../js/ShortCutKeys/WindowShortCutKey.js");
@@ -14,7 +17,9 @@
     const {
         changeShortCutSetting
     } = require("../js/util.js");
-    
+
+    const getWindow = winTitle => BrowserWindow.getAllWindows().find( win => win.getTitle() === winTitle );
+
     ipc.on("akara::shortcutkey", ( evt, location, shKeys ) => {
         console.log("entered ", location);
         let __key_handler_object ;
@@ -25,9 +30,10 @@
             break;
         case "settings":
             __key_handler_object = settingsShortCutKey;
+            ipc.sendTo(getWindow("Settings").webContents.id ,"akara::settings:shortcut", shKeys );
             break;
         case "window":
-            ipc.sendToAll("akara::window:shortcut", windowShortCutKey, shKeys);
+            ipc.sendToAll("akara::window:shortcut", shKeys);
             __key_handler_object = windowShortCutKey;
             break;
         default:
@@ -36,8 +42,8 @@
 
         if ( ! __key_handler_object )
             return;
-        
-        changeShortCutSetting(__key_handler_object);
+
+        changeShortCutSetting(__key_handler_object,shKeys);
 
     });
 })();
