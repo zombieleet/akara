f685fadcbca166ac4d758a31c5694c229f711c6f
zombieleet <zombieleetnca@gmail.com>
2018-11-18 23:18:23 +0100

    automatically remove LOOP_CURRENT_VIDEO from localStorage when user closes the app

--- src/renderer/js/WindowButtonsMainWindow.js
+++ src/renderer/js/WindowButtonsMainWindow.js
@@ -44,6 +44,7 @@
 
     ipc.on("akara::quiting", () => {
         localStorage.removeItem("DONT_SHOW_VOLUME_WARNING");
+        localStorage.removeItem("LOOP_CURRENT_VIDEO");
     });
 
 })(require("electron"));
