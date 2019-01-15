3f21a2dad1625fa2dd5083eb4a81df4dae39efe9
zombieleet <zombieleetnca@gmail.com>
2019-01-15 06:17:31 +0100

    partial implementationof resizing of playlist item section

--- src/renderer/js/AkaraResizeLoader.js
+++ src/renderer/js/AkaraResizeLoader.js
@@ -2,16 +2,27 @@
 
     "use strict";
 
+    const akaraVideoContainer = document.querySelector(".akara-media-cover");
+
+    const video      = akaraVideoContainer.querySelector("video");
     const dragger    = document.querySelector(".akara-loaded-dragger");
     const docElement = document.documentElement;
     const akaraLoad  = document.querySelector(".akara-load");
-    
+
+    console.log("hello world", dragger);
     
     let mouseHeldDown = false;
+    let globalWidth   = parseFloat(getComputedStyle(akaraLoad).width);
 
     const docElementMovement = evt => {
-        const width = evt.clientX;
-        akaraLoad.setAttribute("style", `width: ${width}px`);
+        const playlistItemSectionWidth = ( ( evt.clientX  / akaraLoad.parentNode.clientWidth ) * 100 );
+        const videoContainerWidth      = ( ((( (parseFloat(getComputedStyle(akaraVideoContainer).left) / 1000) + (evt.clientX  / 100)) / 1000) * 100) * 40 );
+
+        console.log(playlistItemSectionWidth, globalWidth, videoContainerWidth);
+        
+        //console.log(akaraVideoContainer.style.marginLeft, "me" , playlistItemSectionWidth , video.clientWidth);
+        akaraVideoContainer.style.left = `${videoContainerWidth}%`;
+        akaraLoad.style.width = `${playlistItemSectionWidth}%`;
     };
 
     dragger.addEventListener("mousedown", evt => {
