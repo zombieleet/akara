8a1a436dbf173c44e28fce35419e721c701923f5
zombieleet <zombieleetnca@gmail.com>
2019-02-12 04:52:29 +0100

    remove trailing whitespace

--- src/renderer/js/Subtitles.js
+++ src/renderer/js/Subtitles.js
@@ -5,12 +5,12 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -99,7 +99,7 @@
         const subtitleParent = document.createElement("table");
 
         let idx = 1;
-        
+
         subtitleParent.appendChild(createTableHeaders(value));
 
         for ( let [ key, values ] of Object.entries(value)) {
@@ -141,7 +141,7 @@
             }
 
             td = document.createElement("td");
-            
+
             if ( keys[i] === "url" ) {
                 __url = _value;
                 i++;
--- src/renderer/js/Util.js
+++ src/renderer/js/Util.js
@@ -6,12 +6,12 @@
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -135,7 +135,7 @@ const createPlaylistItem = ({path: abs_path, _path: rel_path}) => {
         target.after(target, window.__draggingElement);
 
         //const el = window.__draggingElement.outerHTML;
-        
+
         //target.insertAdjacentHTML("beforebegin", el);
         //target.insertAdjacentHTML("afterend", el);
 
@@ -390,7 +390,7 @@ module.exports.disableMenuItem = disableMenuItem;
 const setupPlaying = target => {
 
     const loaded = document.querySelector(".akara-loaded");
-        
+
     localStorage.removeItem("MEDIA_FRAGMENT_LAST");
     localStorage.removeItem("MEDIA_FRAGMENT_FIRST");
 
@@ -1555,10 +1555,8 @@ module.exports.processMediaTags = processMediaTags;
 module.exports.downloadAlbumArt = art => {
 
     dialog.showSaveDialog({
-
         defaultPath: app.getPath("downloads"),
         title: "where to save albumart?"
-
     }, location => {
 
         if ( ! location ) {
