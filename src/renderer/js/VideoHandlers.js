be77bc9246e6e698a130bd19d26246d338fabbce
zombieleet <zombieleetnca@gmail.com>
2019-03-30 02:55:19 +0000

    add support for reading hours in human form

--- src/renderer/js/VideoHandlers.js
+++ src/renderer/js/VideoHandlers.js
@@ -5,12 +5,12 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
@@ -227,9 +227,11 @@ module.exports.removeHoverTime = removeHoverTime;
  *
  **/
 
-const getHumanTime = result => isNaN(result)
-      ? "00:00"
-      : `${(result/60).toFixed(2)}`.replace(/\./, ":");
+const getHumanTime = result => {
+    const date = new Date(null);
+    date.setSeconds(result);
+    return date.toISOString().substr(11,8);
+};
 
 
 
